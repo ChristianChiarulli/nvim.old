784c137fc7d2a6dbf6d736a22e7a24d2a3a8f987
ChristianChiarulli <chris.machine@pm.me>
2021-12-13 17:58:30 -0500

    simple footer

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -25,11 +25,11 @@ dashboard.section.buttons.val = {
 
 local function footer()
 -- NOTE: requires the fortune-mod package to work
-	local handle = io.popen("fortune")
-	local fortune = handle:read("*a")
-	handle:close()
-	return fortune
-	-- return "chrisatmachine.com"
+	-- local handle = io.popen("fortune")
+	-- local fortune = handle:read("*a")
+	-- handle:close()
+	-- return fortune
+	return "chrisatmachine.com"
 end
 
 dashboard.section.footer.val = footer()
