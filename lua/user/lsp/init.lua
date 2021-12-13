fd452b8e174aae0731c912db8361a008969fc3c5
Christian Chiarulli <chris.machine@pm.me>
2021-12-12 21:54:40 -0500

    Initial commit

---
+++ lua/user/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, lspconfig = pcall(require, "lspconfig")
+if not status_ok then
+	return
+end
+
+require("user.lsp.lsp_installer")
+require("user.lsp.handlers").setup()
+require("user.lsp.null-ls")
