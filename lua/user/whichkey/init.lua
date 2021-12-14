814eec97c1dac1a72877a0fbd651fb44677ba4b9
ChristianChiarulli <chris.machine@pm.me>
2021-12-14 07:37:49 -0500

    update float term

--- lua/user/whichkey/init.lua
+++ lua/user/whichkey/init.lua
@@ -236,7 +236,7 @@ local mappings = {
 		u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
 		t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-		f = { "<cmd>ToggleTerm<cr>", "Float" },
+		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
 	},
